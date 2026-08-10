2cdf97132bf28412bc52792387dc650816a4c2f4
Valerii Holub <valiera.golub@gmail.com>
2026-08-10 23:45:34 +0100

    WIP: Design main window

---
+++ LangAppAdminWPFApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangApp.Admin.WPF.ViewModels
+{
+    public sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+
+
+
+
+
+        private void NotifyPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- LangAppAdminWPFApp/Views/MainWindow.xaml.cs
+++ LangAppAdminWPFApp/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using LangApp.Admin.WPF.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,10 @@ namespace LangAppAdminWPFApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainWindowViewModel mainWindowViewModel)
         {
             InitializeComponent();
+            DataContext = mainWindowViewModel;
         }
     }
 }
